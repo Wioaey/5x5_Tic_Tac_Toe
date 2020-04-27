750ba5b3e662a624d943263dbfac9ba9c728207d
leusco <56659698+leusco@users.noreply.github.com>
2020-04-26 19:31:32 -0500

    new: two screens color tiles

---
+++ app/src/main/java/com/example/tictactoe/Board.java
@@ -0,0 +1,146 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Board extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[5][5];
+    private boolean player1turn = true;
+    private int roundCount;
+    private int player1points;
+    private int player2points;
+
+    private TextView txtViewP1;
+    private TextView txtViewP2;
+
+    private int[] colorsArray;
+    private int redColor = 0;
+    private int blueColor = 0;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+        txtViewP1 = findViewById(R.id.text_view_p1);
+        txtViewP2 = findViewById(R.id.text_view_p2);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(Board.this);
+            }
+        }
+
+        colorsArray = getIntent().getIntArrayExtra("colors");
+        unpackColors();
+
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+        roundCount++;
+        if (player1turn) {
+            ((Button) v).setText("X");
+            setRedColor(v);
+            player1turn = false;
+            return;
+        } else {
+            ((Button) v).setText("O");
+            setBlueColor(v);
+            player1turn = true;
+            return;
+        }
+
+
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                field[i][j]  = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][1])) {
+
+            }
+        }
+        return true;
+    }
+
+    private void unpackColors() {
+        redColor = colorsArray[0];
+        blueColor = colorsArray[1];
+    }
+
+    private void setRedColor(View v) {
+        switch (redColor) {
+            case 1:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.red1));
+                break;
+
+            case 2:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.red2));
+                break;
+
+            case 3:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.red3));
+                break;
+
+            case 4:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.red4));
+                break;
+
+            case 5:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.red5));
+                break;
+        }
+    }
+
+    private void setBlueColor(View v) {
+        switch (blueColor) {
+            case 1:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.blue1));
+                break;
+
+            case 2:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.blue2));
+                break;
+
+            case 3:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.blue3));
+                break;
+
+            case 4:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.blue4));
+                break;
+
+            case 5:
+                ((Button) v).setBackgroundTintList(getResources().getColorStateList(R.color.blue5));
+                break;
+
+        }
+    }
+}
--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,77 +1,153 @@
 package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView title;
+    private TextView instructions;
+    private TextView playerOne;
+    private TextView playerTwo;
 
-    private Button[][] buttons = new Button[5][5];
-    private boolean player1turn = true;
-    private int roundCount;
-    private int player1points;
-    private int player2points;
-
-    private TextView txtViewP1;
-    private TextView txtViewP2;
+    private int colorRed = 1;
+    private int colorBlue = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtViewP1 = findViewById(R.id.text_view_p1);
-        txtViewP2 = findViewById(R.id.text_view_p2);
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                String buttonID = "button_" + i + j;
-                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                buttons[i][j] = findViewById(resID);
-                buttons[i][j].setOnClickListener(this);
-            }
-        }
+        title = findViewById(R.id.Title);
+        instructions = findViewById(R.id.Instructions);
+        playerOne = findViewById(R.id.player1);
+        playerTwo = findViewById(R.id.player2);
 
-        Button buttonReset = findViewById(R.id.button_reset);
-        buttonReset.setOnClickListener(new View.OnClickListener() {
+        settingColors(3);
+
+        Button startGame = findViewById(R.id.startGame);
+        startGame.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
+            public void onClick(View view) {
+                int[] colorsArray = new int[2];
+                colorsArray[0] = colorRed;
+                colorsArray[1] = colorBlue;
+                Intent intent = new Intent(view.getContext(), Board.class);
+                intent.putExtra("colors", colorsArray);
+                startActivity(intent);
             }
         });
     }
 
+    private void settingColors(int i) {
+        Button blueOne = findViewById(R.id.blue1);
+        blueOne.setOnClickListener(this);
+        Button blueTwo = findViewById(R.id.blue2);
+        blueTwo.setOnClickListener(this);
+        Button blueThree = findViewById(R.id.blue3);
+        blueThree.setOnClickListener(this);
+        Button blueFour = findViewById(R.id.blue4);
+        blueFour.setOnClickListener(this);
+        Button blueFive = findViewById(R.id.blue5);
+        blueFive.setOnClickListener(this);
+
+        Button redOne = findViewById(R.id.red1);
+        redOne.setOnClickListener(this);
+        Button redTwo = findViewById(R.id.red2);
+        redTwo.setOnClickListener(this);
+        Button redThree = findViewById(R.id.red3);
+        redThree.setOnClickListener(this);
+        Button redFour = findViewById(R.id.red4);
+        redFour.setOnClickListener(this);
+        Button redFive = findViewById(R.id.red5);
+        redFive.setOnClickListener(this);
+
+        if (i == 0) {
+            //reset reds
+            redFive.setText("");
+            redFour.setText("");
+            redThree.setText("");
+            redTwo.setText("");
+            redOne.setText("");
+        }
+        if (i == 1) {
+            //reset blues
+            blueFive.setText("");
+            blueFour.setText("");
+            blueThree.setText("");
+            blueTwo.setText("");
+            blueOne.setText("");
+        }
+    }
+
     @Override
     public void onClick(View v) {
-        if (!((Button) v).getText().toString().equals("")) {
-            return;
-        }
+        switch (v.getId()) {
+            case R.id.red1:
+                settingColors(0);
+                colorRed = 1;
+                ((Button) v).setText("X");
+                break;
 
-        if (player1turn) {
-            ((Button) v).setText("X");
-        } else {
-            ((Button) v).setText("O");
-        }
+            case R.id.red2:
+                settingColors(0);
+                colorRed = 2;
+                ((Button) v).setText("X");
+                break;
 
-        roundCount++;
+            case R.id.red3:
+                settingColors(0);
+                colorRed = 3;
+                ((Button) v).setText("X");
+                break;
+
+            case R.id.red4:
+                settingColors(0);
+                colorRed = 4;
+                ((Button) v).setText("X");
+                break;
+
+            case R.id.red5:
+                settingColors(0);
+                colorRed = 5;
+                ((Button) v).setText("X");
+                break;
+
+                //now blues
+
+            case R.id.blue1:
+                settingColors(1);
+                colorBlue = 1;
+                ((Button) v).setText("X");
+                break;
+
+            case R.id.blue2:
+                settingColors(1);
+                colorBlue = 2;
+                ((Button) v).setText("X");
+                break;
+
+            case R.id.blue3:
+                settingColors(1);
+                colorBlue = 3;
+                ((Button) v).setText("X");
+                break;
+
+            case R.id.blue4:
+                settingColors(1);
+                colorBlue = 4;
+                ((Button) v).setText("X");
+                break;
+
+            case R.id.blue5:
+                settingColors(1);
+                colorBlue = 5;
+                ((Button) v).setText("X");
+                break;
+        }
     }
-
-    private boolean checkForWin() {
-        String[][] field = new String[5][5];
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                field[i][j]  = buttons[i][j].getText().toString();
-            }
-        }
-
-        for (int i = 0; i < 5; i++) {
-            if (field[i][0].equals(field[i][1]) && field[i][0]) {
-
-            }
-        }
-    }
-
 }
+
